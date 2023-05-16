c5a591c55fa257e0a41c11c52e5cf6a72314c403
“Azat” <teronarray@gmail.com>
2023-05-16 11:53:31 +0500

    task completed

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int _variable = 10;
-        int variable_One = 1;
-        int variableTwo = 2;
-        int variable_three = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEfIVE = 5;
-        int VariableSix = 6;
-        int variableseven = 7;
-        int VariableEight = 8;
+        int variable  = 1;
+        int variable_one = 2;
+        int variableTwo = 3;
+        int variable_three = 4;
+        final int VARIABLE_FOUR = 5;
+        final int VARIABLE_FIVE = 6;
+        int variableSix = 7;
+        int variableSeven = 8;
+        int variableEight = 9;
     }
 }
